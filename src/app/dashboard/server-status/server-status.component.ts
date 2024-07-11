e3639743dc50712e6d79d005de215dc7ff9150e6
utkarshsinh <usinh002@ucr.edu>
2024-07-11 13:59:45 -0700

    first commit

---
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.css'
+})
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  constructor(){}
+  ngOnInit(){
+    setInterval(()=>{
+      const rnd = Math.random();
+      if(rnd<0.5){
+        this.currentStatus='online';
+      }
+      else if(rnd<0.9){
+        this.currentStatus='offline';
+      }
+      else{
+        this.currentStatus='unknown';
+      }
+    }, 5000);
+  }
+}
